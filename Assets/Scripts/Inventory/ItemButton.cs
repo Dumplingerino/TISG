a48e24d8dba9a09571ed23b9b6acb6cd2896a53b
Dumplingerino <dumplingerino@gmail.com>
2017-03-20 13:17:23 +0200

    Started adding custom buttons

---
+++ Assets/Scripts/Inventory/ItemButton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ItemButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [SerializeField]
+    private PlayerInteractions player;
+    public int slotIndex;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        player.DropItem(slotIndex);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        for (int i = 0; i < eventData.hovered.Count; i++)
+        {
+            Debug.Log(eventData.hovered[i].name + " hover");
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("No more");
+    }
+}
